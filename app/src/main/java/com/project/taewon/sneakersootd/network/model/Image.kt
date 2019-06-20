d87f06d41185ea4541dbe160e41c716dd1c9e9ad
TktkOkym <taewon.kim@rakuten.com>
2019-06-20 17:37:20 +0900

    fix lint warnings

--- app/src/main/java/com/project/taewon/sneakersootd/network/model/Image.kt
+++ app/src/main/java/com/project/taewon/sneakersootd/network/model/Image.kt
@@ -1,7 +1,6 @@
 package com.project.taewon.sneakersootd.network.model
 
 import android.os.Parcelable
-import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
